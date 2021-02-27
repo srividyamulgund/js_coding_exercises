e0a382488d18fc694ad754bc85baa8050b999adc
Srividya Mulgund <srividya.mulgund@gmail.com>
2021-02-27 07:42:10 +0000

    Implemented Exercise 2

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,47 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+
+  return (person.city == "Manchester" ? true : false);
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+
+  let count = 0;
+  let divisor = 40;
+
+  let rem = people % divisor;
+  let quo = people / divisor;
+
+  if (rem > 0) {
+    count = Math.floor(quo + 1);
+  }
+  else {
+    count = Math.floor(quo);
+  }
+  return count;
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+
+  var search = "sheep";
+  var occurrences = arr.filter(function (val) {
+    return val === search;
+  }).length;
+  return (occurrences);
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+
+  return (((person.address.postCode.charAt(0) == "M") && person.address.city == "Manchester") ? true : false);
 }
 
 module.exports = {
